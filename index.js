17345f14684a3737c59e576c0f2b74554a26750a
Lucas <lucasmazucosilva@gmail.com>
2021-11-18 20:49:33 -0300

    primeiro commit

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ const nota = require("./rotas/nota");
 app.use("/usuario", usuario);
 app.use("/nota", nota);
 
-app.listen(porta, function () {
+app.listen(porta, () => {
     console.log(`Aplicação rodando na porta ${porta} `);
 })
