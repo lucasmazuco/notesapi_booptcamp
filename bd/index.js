e6fc6ee47d1b84eeff7464ed76f75fdb4c35f6d6
Lucas <lucasmazucosilva@gmail.com>
2021-11-18 21:43:28 -0300

    terceiro commit

---
+++ bd/index.js
@@ -0,0 +1,11 @@
+const {Sequelize} = require("sequelize");
+const bd = {};
+
+const options = {
+    username: "admin",
+    password: "notes123",
+    database: "notes",
+    dialect: "mysql",
+    host: "nnotes.cgssmrnlwpdu.us-east-2.rds.amazonaws.com"
+};
+
