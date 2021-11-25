54dd464d5847dc9533cb7041d880486210490366
Lucas <lucasmazucosilva@gmail.com>
2021-11-25 20:00:55 -0300

    a

--- Rotas/usuario.js
+++ Rotas/usuario.js
@@ -1,20 +1,23 @@
-const {Router}= require("express");
+const { Router } = require("express");
+const { Usuario } = require("../bd");
 const router = Router();
 
-router.get("/",(req, res) => {
-    res.send([]);
+router.get("/", (req, res) => {
+  res.send([]);
 });
 
-router.post("/", (req, res) => {
-    res.send({});
+router.post("/", async (req, res) => {
+  const usuario = await Usuario.create(req.body);
+
+  res.send(usuario);
 });
 
 router.put("/:id", (req, res) => {
-    res.send({});
+  res.send({});
 });
 
 router.delete("/:id", (req, res) => {
-    res.send({});
+  res.send({});
 });
 
 module.exports = router;
